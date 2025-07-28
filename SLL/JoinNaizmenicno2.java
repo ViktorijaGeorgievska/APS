a50fdaab77a502be7b274f7db50cdc3afb94a700
Viktorija <viktorijageorgievska4@gmail.com>
2025-07-29 01:58:46 +0200

    Update JoinNaizmenicno2.java

--- SLL/JoinNaizmenicno2.java
+++ SLL/JoinNaizmenicno2.java
@@ -10,47 +10,54 @@ Input:              Output:
 */
 
 public class JoinNazimenicno2 {
-    public static void main(String[] args) {
-        SLL<Integer> firstList = new SLL<>();
-        SLL<Integer> secondList = new SLL<>();
+    public static void join (SLL<Integer> list1, SLL<Integer> list2) {
         SLL<Integer> result = new SLL<>();
+        SLLNode<Integer> current1 = list1.getFirst();
+        SLLNode<Integer> current2 = list2.getFirst();
 
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        for (int i = 0; i < n; i++) {
-            firstList.insertLast(in.nextInt());
-        }
-        int m = in.nextInt();
-        for (int i = 0; i < m; i++) {
-            secondList.insertLast(in.nextInt());
-        }
-        in.close();
-
-        SLLNode<Integer> current1 = firstList.getFirst();
-        SLLNode<Integer> current2 = secondList.getFirst();
-
-        while (current1.succ != null && current2.succ != null) {
+        while (current1 != null && current2 != null) {
             for (int i = 0; i < 2; i++) {
+                if (current1 == null)
+                    break;
                 result.insertLast(current1.element);
                 current1 = current1.succ;
             }
             for (int i = 0; i < 2; i++) {
+                if (current2 == null)
+                    break;
                 result.insertLast(current2.element);
                 current2 = current2.succ;
             }
-        }
-        if (current1 != null) {
-            while (current1 != null) {
+
+            while (current1 != null)
+            {
                 result.insertLast(current1.element);
                 current1 = current1.succ;
             }
-        }
-        if (current2 != null) {
-            while (current2 != null) {
+            while (current2 != null)
+            {
                 result.insertLast(current2.element);
                 current2 = current2.succ;
             }
         }
         System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        SLL<Integer> list1 = new SLL<>();
+        SLL<Integer> list2 = new SLL<>();
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            list1.insertLast(in.nextInt());
+        }
+        int m = in.nextInt();
+        for (int i = 0; i < m; i++) {
+            list2.insertLast(in.nextInt());
+        }
+        in.close();
+
+        join(list1, list2);
     }
 }
