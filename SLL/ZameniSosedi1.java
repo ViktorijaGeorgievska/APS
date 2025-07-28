fa5b614dcae0dc15d30c69752b962fa8c4522dad
Viktorija <viktorijageorgievska4@gmail.com>
2025-07-28 16:37:25 +0200

    Update ZameniSosedi1.java

--- SLL/ZameniSosedi1.java
+++ SLL/ZameniSosedi1.java
@@ -7,25 +7,34 @@ Input:         Output:
 4 6 4 9 3
 */
 
-public class ZameniSosedi1 {
+public class SwapPairs1 {
+    public static void transformList(SLL<Integer> list) {
+        SLLNode<Integer> first = list.getFirst();
+
+        while (first != null && first.succ != null) {
+            SLLNode<Integer> second = first.succ;
+ 
+            Integer helper = first.element;             // чувај вредност на first
+            first.element = second.element;             // вредноста на first нека биде вредноста на наредниот (second)
+            second.element = helper;                    // вредноста на наредниот (second) нека биде вредноста на first
+            
+            first = first.succ.succ;
+        }
+        System.out.println(list);
+    }
+
     public static void main(String[] args) {
         SLL<Integer> list = new SLL<>();
-
+        
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         for (int i = 0; i < n; i++) {
             list.insertLast(input.nextInt());
         }
         input.close();
-
-        SLLNode<Integer> current = list.getFirst();
-        while (current != null && current.succ != null) {
-            Integer tmp = current.element;                        // чувај вредност на current
-            current.element = current.succ.element;               // вредноста на current нека биде вредноста на наредниот
-            current.succ.element = tmp;                           // вредноста на наредниот нека биде вредноста на current
-
-            current = current.succ.succ;
-        }
-        System.out.println(list);
+        
+        transformList(list);
     }
 }
+
+
