b7857b21e8e9192b63aa499007bc9d0f30ed67d7
Viktorija <viktorijageorgievska4@gmail.com>
2025-07-31 02:21:07 +0200

    Update ParitySplit1.java

--- DLL/ParitySplit1.java
+++ DLL/ParitySplit1.java
@@ -8,44 +8,45 @@ Input:                Output:
 */
 
 public class ParitySplit1 {
+    public static void split(DLL<Integer> list) {
+        DLL<Integer> evenList = new DLL<>();
+        DLL<Integer> oddList = new DLL<>();
+        DLLNode<Integer> first = list.getFirst();
+        DLLNode<Integer> last = list.getLast();
+
+        while (first != last) {
+            if (first.element % 2 == 0)
+                evenList.insertLast(first.element);
+            else
+                oddList.insertLast(first.element);
+
+            if (last.element % 2 == 0)
+                evenList.insertLast(last.element);
+            else
+                oddList.insertLast(last.element);
+
+            first = first.succ;
+            last = last.pred;
+        }
+        // za first == last
+        if (first.element % 2 == 0)
+            evenList.insertLast(first.element);
+        else
+            oddList.insertLast(first.element);
+        
+        System.out.println(evenList);
+        System.out.println(oddList);
+    }
+    
     public static void main(String[] args) {
         DLL<Integer> list = new DLL<>();
 
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
             list.insertLast(in.nextInt());
-        }
         in.close();
 
-        DLL<Integer> parna = new DLL<>();
-        DLL<Integer> neParna = new DLL<>();
-        DLLNode<Integer> first = list.getFirst();
-        DLLNode<Integer> last = list.getLast();
-
-        while (first != last && last.succ != first) {
-            if (first.element % 2 == 0) {
-                parna.insertLast(first.element);
-            } else {
-                neParna.insertLast(first.element);
-            }
-            first = first.succ;
-            if (last.element % 2 == 0) {
-                parna.insertLast(last.element);
-            } else {
-                neParna.insertLast(last.element);
-            }
-            last = last.pred;
-        }
-        if (first == last) {
-            if (first.element % 2 == 0) {
-                parna.insertLast(first.element);
-            }
-            else {
-                neParna.insertLast(first.element);
-            }
-        }
-        System.out.println(parna);
-        System.out.println(neParna);
+        split(list);
     }
 }
