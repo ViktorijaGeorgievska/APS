12d7fafeb55f1c0c8f20c2e622c9d27e6265ed2d
Viktorija <viktorijageorgievska4@gmail.com>
2025-07-31 14:39:43 +0200

    Update AverageSplit2.java

--- DLL/AverageSplit2.java
+++ DLL/AverageSplit2.java
@@ -8,49 +8,54 @@ Input:          Output:
 */
 
 public class AverageSplit2 {
+    public static int average (DLL<Integer> list) {
+        DLLNode<Integer> current = list.getFirst();
+        int sum = 0;
+
+        while (current != null) {
+            sum += current.element;
+            current = current.succ;
+        }
+        return sum / list.getSize();
+    }
+
+    public static void print (DLL<Integer> list) {
+        DLLNode<Integer> current = list.getFirst();
+        while (current != null) {
+            System.out.print(current.element + " ");
+            current = current.succ;
+        }
+    }
+
+    public static void splitByAverage(DLL<Integer> list) {
+        int average = average(list);
+        DLLNode<Integer> current = list.getLast();
+        DLL<Integer> lower = new DLL<>();
+        DLL<Integer> upper = new DLL<>();
+
+        while (current != null) {
+            if (current.element <= average) {
+                lower.insertLast(current.element);
+            }
+            else {
+                upper.insertLast(current.element);
+            }
+            current = current.pred;
+        }
+        print(lower);
+        System.out.println();
+        print(upper);
+    }
+
     public static void main(String[] args) {
         DLL<Integer> list = new DLL<>();
 
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             list.insertLast(in.nextInt());
-        }
         in.close();
 
-        DLL<Integer> lowerThanAverage = new DLL<>();
-        DLL<Integer> graterThanAverage = new DLL<>();
-
-        DLLNode<Integer> current = list.getFirst();
-        int sum = 0;
-        while (current != null) {
-            sum += current.element;
-            current = current.succ;
-        }
-        int average = sum / n;
-
-        current = list.getLast();
-        while (current != null) {
-            if (current.element <= average) {
-                lowerThanAverage.insertLast(current.element);
-            } else {
-                graterThanAverage.insertLast(current.element);
-            }
-            current = current.pred;
-        }
-
-        current = lowerThanAverage.getFirst();
-        while (current != null) {
-            System.out.print(current.element + " ");
-            current = current.succ;
-        }
-        System.out.println();
-
-        current = graterThanAverage.getFirst();
-        while (current != null) {
-            System.out.print(current.element + " ");
-            current = current.succ;
-        }
-        System.out.println();
+        splitByAverage(list);
     }
 }
