b3b1bd73024a6ddcca7361d35f8658b24fe2c938
Viktorija <viktorijageorgievska4@gmail.com>
2025-07-31 02:29:00 +0200

    Update InsertAverage1.java

--- DLL/InsertAverage1.java
+++ DLL/InsertAverage1.java
@@ -7,26 +7,28 @@ Input:           Output:
 */
 
 public class InsertAverage1 {
+    public static void insertAvg(DLL<Integer> list) {
+        DLLNode<Integer> first = list.getFirst();
+
+        while (first.succ != null) {
+            DLLNode<Integer> next = first.succ;
+            int avg = (Math.round((float) (first.element + next.element) / 2));
+
+            list.insertBefore(avg, next);
+            first = next;
+        }
+        System.out.println(list);
+    }
+    
     public static void main(String[] args) {
         DLL<Integer> list = new DLL<>();
 
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
             list.insertLast(in.nextInt());
-        }
         in.close();
-
-        DLLNode<Integer> first = list.getFirst();
-        DLLNode<Integer> second = first.succ;
-
-        float avg;
-        while (first != null && second != null) {
-            avg = Math.round((float) (first.element + second.element) / 2);
-            list.insertBefore((int) avg, second);
-            first = first.succ;
-            second = second.succ;
-        }
-        System.out.println(list);
+        
+        insertAvg(list);
     }
 }
