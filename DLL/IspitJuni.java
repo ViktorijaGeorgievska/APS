9ff82fba75c3026cbe769d09f1ac981c517b6a42
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-01 12:49:46 +0200

    Update IspitJuni.java

--- DLL/IspitJuni.java
+++ DLL/IspitJuni.java
@@ -1,36 +1,30 @@
 import java.util.Scanner;
-/*
-7
-TYPE h
-TYPE e
-UNDO
-TYPE l
-SHOW
-TYPE l
-SHOW
 
-hll
-*/
+public class ExamJune {
+    public static void print (DLL<Character> list) {
+        DLLNode<Character> current = list.getFirst();
+        while (current != null) {
+            System.out.print(current.element);
+            current = current.succ;
+        }
+        System.out.println();
+    }
 
-public class IspitJuni {
     public static void main(String[] args) {
-        DLL<String> list = new DLL<>();
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        for (int i = 0; i < n; i++) {
-            if (input.next().equals("TYPE")) {
-                list.insertLast(input.next());
-            } else if (input.next().equals("UNDO")) {
+        DLL<Character> list = new DLL<>();
+
+        Scanner in = new Scanner(System.in);
+        int numLines = in.nextInt();
+        in.nextLine();
+        for (int i = 0; i < numLines; i++) {
+            String []parts = in.nextLine().split(" ");
+
+            if (parts[0].equals("TYPE"))
+                list.insertLast(parts[1].charAt(0));
+            else if (parts[0].equals("UNDO"))
                 list.deleteLast();
-            } else if (input.next().equals("SHOW")) {
-                StringBuilder sb = new StringBuilder();
-                DLLNode<String> temp = list.getFirst();
-                while (temp != null) {
-                    sb.append(temp);
-                    temp = temp.succ;
-                }
-                System.out.println(sb);
-            }
+            else if (parts[0].equals("SHOW"))
+                print(list);
         }
     }
 }
