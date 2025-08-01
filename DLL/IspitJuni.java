ccb5bcb72e6d25bf15e7f5be9633b10dbe5bf7ce
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-01 12:51:45 +0200

    Update IspitJuni.java

--- DLL/IspitJuni.java
+++ DLL/IspitJuni.java
@@ -1,5 +1,52 @@
 import java.util.Scanner;
 
+/*
+Input:
+34
+TYPE H
+TYPE e
+TYPE l
+TYPE l
+TYPE o
+SHOW
+TYPE o
+TYPE o
+TYPE o
+SHOW
+UNDO
+UNDO
+UNDO
+TYPE _
+TYPE t
+TYPE h
+TYPE e
+TYPE e
+UNDO
+TYPE r
+TYPE e
+SHOW
+TYPE !
+SHOW
+TYPE !
+TYPE !
+TYPE !
+SHOW
+TYPE 1
+TYPE 1
+SHOW
+UNDO
+UNDO
+SHOW
+Output:
+Hello
+Helloooo
+Hello_there
+Hello_there!
+Hello_there!!!!
+Hello_there!!!!11
+Hello_there!!!!
+*/
+
 public class ExamJune {
     public static void print (DLL<Character> list) {
         DLLNode<Character> current = list.getFirst();
