52eeb5de9b37519c04b69a0756b5a76e4b560843
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-14 14:51:59 +0200

    Update Molecules2.java

--- Stack/Molecules2.java
+++ Stack/Molecules2.java
@@ -1,67 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /*
 APS book
-Input: H H O H H O H H O H H H H H O H O H O O H O O H H H
-Output: 8  H  0
+Input:
+H H O H H O H H O H H H H H O H O H O O H O O H H H
+Output:
+8  
+H  
+0
 */
 
 public class Molecules2 {
-    public static String makeWater(SLL<String> list) {
-        ArrayStack<String> hStack = new ArrayStack<>(50);
-        ArrayStack<String> oStack = new ArrayStack<>(50);
+    public static void makeWater(SLL<String> letters) {
+        ArrayStack<String> stackH = new ArrayStack<>(50);
+        ArrayStack<String> stackO = new ArrayStack<>(50);
 
-        int num = 0;
-        String result = "";
-        SLLNode<String> current = list.getFirst();
+        SLLNode<String> current = letters.getFirst();
+        int numMolecules = 0;
 
         while (current != null) {
-            String element = current.element;
-            if (element.equals("O")) {
-                oStack.push(element);
-            } else {
-                hStack.push(element);
+            if (current.element.equals("H"))
+                stackH.push(current.element);
+            else if (current.element.equals("O"))
+                stackO.push(current.element);
+
+            if (stackH.size() >= 2 && !stackO.isEmpty()) {
+                stackH.pop();
+                stackH.pop();
+                stackO.pop();
+                numMolecules++;
             }
             current = current.succ;
         }
-
-        while (!oStack.isEmpty()) {
-            if (!hStack.isEmpty()) {
-                hStack.pop();
-                if (!hStack.isEmpty()) {
-                    hStack.pop();
-                    oStack.pop();
-                    num++;
-                } else {
-                    hStack.push("H");
-                    break;
-                }
-            } else {
-                break;
-            }
-        }
-        while (!hStack.isEmpty()) {
-            hStack.pop();
-            result += "H\n";
-        }
-        while (!oStack.isEmpty()) {
-            oStack.pop();
-            result += "O\n";
-        }
-        System.out.println(num);
-        return result;
+        
+        System.out.println(numMolecules);
+        if (!stackH.isEmpty())
+            while (!stackH.isEmpty())
+                System.out.println(stackH.pop());
+        if (!stackO.isEmpty())
+            while (!stackO.isEmpty())
+                System.out.println(stackO.pop());
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = in.readLine().split(" ");
-        SLL<String> list = new SLL<>();
-        for (int i = 0; i < input.length; i++) {
-            list.insertLast(input[i]);
-        }
-        in.close();
-        System.out.println(makeWater(list));
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        SLL<String> letters = new SLL<>();
+        String[] line = input.nextLine().split(" ");
+        for (int i = 0; i < line.length; i++)
+            letters.insertLast(line[i]);
+        input.close();
+
+        makeWater(letters);
     }
 }
