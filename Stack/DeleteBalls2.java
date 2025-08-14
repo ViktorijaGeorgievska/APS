f5d18d80b1c37d52d96954659822ed9f15c0dab3
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-14 13:40:52 +0200

    Update DeleteBalls2.java

--- Stack/DeleteBalls2.java
+++ Stack/DeleteBalls2.java
@@ -1,106 +1,90 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /*
 APS book
-Input: R+ G- G+ G+ R+ B- B+ R- G+ R- B- B+ B+ R+
+Input:
+R+ G- G+ G+ R+ B- B+ R- G+ R- B- B+ B+ R+
 Output:
-4
-R- B- G- G-
+5
+R+ G+ G+ B+
 */
 
 public class DeleteBalls2 {
-    public static String deleteBalls(SLL<String> list) {
-        LinkedStack<String> redStack = new LinkedStack<>();
-        LinkedStack<String> blueStack = new LinkedStack<>();
-        LinkedStack<String> greenStack = new LinkedStack<>();
+    public static void deleteBalls(SLL<String> balls) {
+        ArrayStack<String> redStack = new ArrayStack<>(50);
+        ArrayStack<String> blueStack = new ArrayStack<>(50);
+        ArrayStack<String> greenStack = new ArrayStack<>(50);
 
+        SLLNode<String> current = balls.getFirst();
         int numPairs = 0;
-        String result = "";
-        SLLNode<String> current = list.getFirst();
+        StringBuilder alone = new StringBuilder();
 
         while (current != null) {
-            String ball = current.element;
-            char color = ball.charAt(0);
-            char sign = ball.charAt(1);
+            char color = current.element.charAt(0);
+            char sign = current.element.charAt(1);
 
             if (color == 'R') {
-                if (!redStack.isEmpty()) {
-                    if (redStack.peek().equals(ball)) {
-                        redStack.push(ball);
-                    }
-                    else {
+                if (redStack.isEmpty())
+                    redStack.push(color + "" + sign);
+                else {
+                    String inStack = redStack.peek();
+                    if (inStack.equals(color + "" + sign))        // ако се исти, стави на стек
+                        redStack.push(color + "" + sign);
+                    else {                                        // ако се различни, извади од стек
                         redStack.pop();
+                        numPairs++;
                     }
                 }
+            } else if (color == 'B') {
+                if (blueStack.isEmpty())
+                    blueStack.push(color + "" + sign);
                 else {
-                    redStack.push(ball);
-                }
-            }
-            if (color == 'B') {
-                if (!blueStack.isEmpty()) {
-                    if (blueStack.peek().equals(ball)) {
-                        blueStack.push(ball);
-                    } else {
+                    String inStack = blueStack.peek();
+                    if (inStack.equals(color + "" + sign))
+                        blueStack.push(color + "" + sign);
+                    else {
                         blueStack.pop();
+                        numPairs++;
                     }
                 }
+            } else if (color == 'G') {
+                if (greenStack.isEmpty())
+                    greenStack.push(color + "" + sign);
                 else {
-                    blueStack.push(ball);
-                }
-            }
-            if (color == 'G') {
-                if (!greenStack.isEmpty()) {
-                    if (greenStack.peek().equals(ball)) {
-                        greenStack.push(ball);
-                    } else {
+                    String inStack = greenStack.peek();
+                    if (inStack.equals(color + "" + sign))
+                        greenStack.push(color + "" + sign);
+                    else {
                         greenStack.pop();
+                        numPairs++;
                     }
-                }
-                else {
-                    greenStack.push(ball);
                 }
             }
             current = current.succ;
         }
-        while (!redStack.isEmpty()) {
-            numPairs++;
-            if (redStack.pop().charAt(1) == '+') {
-                result += "R- ";
-            } else {
-                result += "R+ ";
-            }
-        }
-        while (!blueStack.isEmpty()) {
-            numPairs++;
-            if (blueStack.pop().charAt(1) == '+') {
-                result += "B- ";
-            } else {
-                result += "B+ ";
-            }
-        }
-        while (!greenStack.isEmpty()) {
-            numPairs++;
-            if (greenStack.pop().charAt(1) == '+') {
-                result += "G- ";
-            } else {
-                result += "G+ ";
-            }
-        }
+        if (!redStack.isEmpty())
+            while (!redStack.isEmpty())
+                alone.append(redStack.pop() + " ");
+        if (!greenStack.isEmpty())
+            while (!greenStack.isEmpty())
+                alone.append(greenStack.pop() + " ");
+        if (!blueStack.isEmpty())
+            while (!blueStack.isEmpty())
+                alone.append(blueStack.pop() + " ");
+
         System.out.println(numPairs);
-        return result;
+        System.out.println(alone);
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] line = input.readLine().split(" ");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
         SLL<String> balls = new SLL<>();
-        for (int i = 0; i < line.length; i++) {
+        String[] line = input.nextLine().split(" ");
+        for (int i = 0; i < line.length; i++)
             balls.insertLast(line[i]);
-        }
-        System.out.println(deleteBalls(balls));
+
+        deleteBalls(balls);
     }
 }
+
