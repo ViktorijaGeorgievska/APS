0482aff28b6c31b17dd5d4c60c0aac292026541e
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-14 12:39:49 +0200

    Update CalcPostfix1.java

--- Stack/CalcPostfix1.java
+++ Stack/CalcPostfix1.java
@@ -3,57 +3,50 @@ import java.util.Scanner;
 /*
 APS book basic
 Input: 5 9 + 2 * 6 5 * +
-Output: Rezultatot e 58.0
+Output: he result is: 58.0
 */
 
 public class CalcPostfix1 {
-    public static Double postfix(String expression) {
-        LinkedStack<Double> stack = new LinkedStack<>();
-        Double r = null;
+    public static void calculate(String expression) {
+        ArrayStack<Float> calcStack = new ArrayStack<>(50);
 
         for (int i = 0; i < expression.length(); i++) {
-            char current = expression.charAt(i);
-            if (current == ' ')
+            char c = expression.charAt(i);
+            if (c == ' ')
                 continue;
-            else if (Character.isDigit(current)) {
-                stack.push((double) current - '0');
-            }
+            else if (Character.isDigit(c))
+                calcStack.push((float) c - '0');
             else {
-                if (stack.size() >= 2) {
-                    Double lastNum = stack.pop();
-                    Double beforeLastNum = stack.pop();
-                    if (current == '+') {
-                        stack.push(beforeLastNum + lastNum);
-                    }
-                    else if (current == '-') {
-                        stack.push(beforeLastNum - lastNum);
-                        break;
-                    }
-                    else if (current == '*') {
-                        stack.push(beforeLastNum * lastNum);
-                    }
-                    else if (current == '/') {
-                        stack.push(beforeLastNum / lastNum);
-                    }
-                    else {
-                        System.out.println("Nevaliden vlez - nedostasuva operand na pozicija: " + i);
-                    }
+                if (calcStack.size() >= 2) {
+                    float num1 = calcStack.pop();
+                    float num2 = calcStack.pop();
+
+                    if (c == '+')
+                        calcStack.push(num1 + num2);
+                    else if (c == '-')
+                        calcStack.push(num1 - num2);
+                    else if (c == '*')
+                        calcStack.push(num1 * num2);
+                    else if (c == '/')
+                        calcStack.push(num1 / num2);
+                    else
+                        System.out.println("The expression is not valid! Missing operand on position " + i + ".");
                 }
             }
         }
-        if (stack.size() != 1) {
-            System.out.println("Nevaliden vlez - nedostasuva operator");
-        }
-        else {
-            r = stack.pop();
-        }
-        return r;
+        if (calcStack.size() != 1)
+            System.out.println("The expression is not valid! Missing operator!");
+        else
+            System.out.println("The result is: " + calcStack.pop());
     }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String expression = in.nextLine();
+        Scanner input = new Scanner(System.in);
+        String expression = input.nextLine();
 
-        System.out.println("Rezultatot e: " + postfix(expression));
+        calculate(expression);
     }
 }
+
+
+
