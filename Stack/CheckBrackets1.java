533c65402d969b63f60d97412999a8d3f597e2fc
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-14 12:21:58 +0200

    Update CheckBrackets1.java

--- Stack/CheckBrackets1.java
+++ Stack/CheckBrackets1.java
@@ -1,78 +1,71 @@
 import java.util.Scanner;
+
 /*
 APS book basic
 {s x [s - a] x (s - b) x (s - c)}
 {} [s x (s - a) x s - b) x (s - c)
 */
 public class CheckBrackets1 {
-    public static boolean daliKorektni (String expression) {
-        ArrayStack<Character> bracketStack = new ArrayStack<>(100);
+    public static boolean areCorrect(String expression) {
+        ArrayStack<Character> bracketStack = new ArrayStack<>(50);
 
         for (int i = 0; i < expression.length(); i++) {
-            char currentChar = expression.charAt(i);
-            if (currentChar == '(' || currentChar == '{' || currentChar == '[') {
-                bracketStack.push(currentChar);
-            }
-            else if (currentChar == ')' || currentChar == '}' || currentChar == ']') {
+            char c = expression.charAt(i);
+            if (c == '{' || c == '[' || c == '(')
+                bracketStack.push(c);
+            else if (c == '}' || c == ']' || c == ')') {
                 if (bracketStack.isEmpty())
                     return false;
-
-                char leftBracket = bracketStack.pop();
-                if (!daliSoodvetni(leftBracket, currentChar))
+                else if (!checkSameBrackets(bracketStack.pop(), c))
                     return false;
             }
         }
-        return bracketStack.isEmpty();      // ако е empty ќе врати true
+        return bracketStack.isEmpty();            // ако е empty значи сите се во ред и ќе врати true
     }
 
-    public static boolean daliSoodvetni(char leftBracket, char rightBracket) {
-        switch (leftBracket) {
-            case '(':
-                return (rightBracket == ')');
-            case '[':
-                return (rightBracket == ']');
-            case '{':
-                return (rightBracket == '}');
-        }
+    public static boolean checkSameBrackets(char leftBracket, char rightBracket) {
+        if (leftBracket == '(' && rightBracket == ')')
+            return true;
+        else if (leftBracket == '{' && rightBracket == '}')
+            return true;
+        else if (leftBracket == '[' && rightBracket == ']')
+            return true;
         return false;
     }
 
-    public static boolean checkBrackets (String expression) {
+    public static boolean checkBrackets(String expression) {
         LinkedStack<Character> stack = new LinkedStack<>();
 
         for (int i = 0; i < expression.length(); i++) {
             if (expression.charAt(i) == '(' || expression.charAt(i) == '{' || expression.charAt(i) == '[') {
                 stack.push(expression.charAt(i));
-            }
-            else if (expression.charAt(i) == ')') {
+            } else if (expression.charAt(i) == ')') {
                 if (stack.isEmpty())
                     return false;
                 if (stack.pop() != '(')
                     return false;
-            }
-            else if (expression.charAt(i) == ']') {
+            } else if (expression.charAt(i) == ']') {
                 if (stack.isEmpty())
                     return false;
                 if (stack.pop() != '[')
                     return false;
-            }
-            else if (expression.charAt(i) == '}') {
+            } else if (expression.charAt(i) == '}') {
                 if (stack.isEmpty())
                     return false;
                 if (stack.pop() != '{')
                     return false;
             }
         }
-        if (stack.isEmpty())
-            return true;
-        else
-            return false;
+        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String expression = in.nextLine();
+        Scanner input = new Scanner(System.in);
+        String expression = input.nextLine();
 
-        System.out.println(expression + " ima " + (daliKorektni(expression) ? "korektni" : "nekorektni") + " zagradi.");
+        System.out.println(expression + " have " + (areCorrect(expression) ? "correct" : "not correct") + " brackets!");
     }
 }
+
+   
+  
