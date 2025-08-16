d9d3d1b3aa95760bf2aa1e46bfb01c4ac0b6ffd4
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-16 19:31:57 +0200

    Update RoundRobin1.java

--- Queue/RoundRobin1.java
+++ Queue/RoundRobin1.java
@@ -1,96 +1,103 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.Scanner;
 
 /*
 APS book basic
-Input:         Output:
-5              B A E D C B A E D C B A E D C B A E D D
+Input:
+5
 A 40 2
 B 35 0
 C 28 10
 D 45 4
 E 32 2
 10
+Output:
+B A E D C B A E D C B A E D C B A E D D
 */
 
 class Process implements Comparable<Process> {
-    private String name;
-    private int arrivalTime, executionTime;
+    String name;
+    int executionTime;
+    int arrivalTime;
 
-    public Process(String name, int arrivalTime, int executionTime) {
+    public Process(String name, int executionTime, int arrivalTime) {
         this.name = name;
-        this.arrivalTime = arrivalTime;
         this.executionTime = executionTime;
+        this.arrivalTime = arrivalTime;
     }
 
-    public void updateTime(int quantum) {
-        if (quantum > this.executionTime) {
+    public void updateExecutionTime(int quantum) {
+        if (quantum > this.executionTime)
             this.executionTime = 0;
-        } else {
+        else
             this.executionTime -= quantum;
-        }
-    }
-
-    public int getArrivalTime() {
-        return this.arrivalTime;
-    }
-
-    public int getExecutionTime() {
-        return this.executionTime;
-    }
-
-    public String toString() {
-        return name;
     }
 
     @Override
     public int compareTo(Process o) {
-        if (this.arrivalTime > o.getArrivalTime())
-            return 1;
-        else if (this.arrivalTime == o.getArrivalTime())
-            if (this.executionTime > o.getExecutionTime())
-                return -1;
-            else
-                return 1;
-        else
-            return -1;
+        if (this.arrivalTime != o.arrivalTime)
+            return Integer.compare(this.arrivalTime, o.arrivalTime);
+        return Integer.compare(this.executionTime, o.executionTime);
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
 
 public class RoundRobin1 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        LinkedList<Process> processes = new LinkedList<>();
-        Process p;
-        LinkedQueue<Process> queue = new LinkedQueue<>();
+    public static void sortList(SLL<Process> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            SLLNode<Process> current = list.getFirst();
+            SLLNode<Process> next = current.succ;
 
-        int n = Integer.parseInt(input.readLine());
-        String []lines;
-        for (int i = 0; i < n; i++) {
-            lines = input.readLine().split(" ");
-            String name = lines[0];
-            int executionTime = Integer.parseInt(lines[1]);
-            int arrivalTime = Integer.parseInt(lines[2]);
-
-            p = new Process(name, arrivalTime, executionTime);
-            processes.add(p);
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                if (current.element.arrivalTime > next.element.arrivalTime) {
+                    Process temp = current.element;
+                    current.element = next.element;
+                    next.element = temp;
+                }
+                current = next;
+                next = current.succ;
+            }
         }
-        int quantum = Integer.parseInt(input.readLine());
+    }
 
-        Collections.sort(processes);
-        for (int i = 0; i < n; i++) {
-            queue.enqueue(processes.get(i));
+    public static void implementRoundRobin(SLL<Process> list, int quantum) {
+        ArrayQueue<Process> processesQueue = new ArrayQueue<>(50);
+        SLLNode<Process> current = list.getFirst();
+
+        while (current != null) {
+            processesQueue.enqueue(current.element);
+            current = current.succ;
         }
 
-        while (!queue.isEmpty()) {
-            p = queue.dequeue();
-            p.updateTime(quantum);
-            if (p.getExecutionTime() != 0)
-                queue.enqueue(p);
+        while (!processesQueue.isEmpty()) {
+            Process p = processesQueue.dequeue();
+            p.updateExecutionTime(quantum);
+
+            if (p.executionTime != 0)
+                processesQueue.enqueue(p);
+
             System.out.print(p + " ");
         }
+    }
+
+    public static void main(String[] args) {
+        SLL<Process> list = new SLL<>();
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        input.nextLine();
+        for (int i = 0; i < n; i++) {
+            String[] line = input.nextLine().split(" ");
+            Process p = new Process(line[0], Integer.parseInt(line[1]), Integer.parseInt(line[2]));
+            list.insertLast(p);
+        }
+        int quantum = input.nextInt();
+        input.close();
+
+        sortList(list);
+        implementRoundRobin(list, quantum);
     }
 }
