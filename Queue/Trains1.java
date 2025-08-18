3774c0338839994e51ff85d0fac6ccd400aa79c0
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-18 19:17:13 +0200

    Update Trains1.java

--- Queue/Trains1.java
+++ Queue/Trains1.java
@@ -5,89 +5,86 @@ import java.io.InputStreamReader;
 /*
 APS book basic
 Input:
-6
+10
 3
+0
 0
 7
 0
 1
 2
-Output: 1 2 3 7
+0
+0
+10
+Output:
+1 2 3 7 10
 */
+
 public class Trains1 {
-    public static String preRaspredeleni(String[] lines) {
-        LinkedStack<Integer> oldTrain = new LinkedStack<>();
-        LinkedStack<Integer> newTrain = new LinkedStack<>();
-        LinkedQueue<Integer> shina = new LinkedQueue<>();
-
+    public static StringBuilder preRaspredeleni(String[] input) {
+        ArrayStack<Integer> oldTrain = new ArrayStack<>(input.length);
+        ArrayStack<Integer> newTrain = new ArrayStack<>(input.length);
+        ArrayQueue<Integer> shina = new ArrayQueue<>(input.length);
         int top;
-        String out = new String("");
+        StringBuilder out = new StringBuilder();
 
-        for (int i = 0; i < lines.length; i++) {
-            oldTrain.push(Integer.parseInt(lines[i]));
-        }
+        for (int i = 0; i < input.length; i++)
+            oldTrain.push(Integer.parseInt(input[i]));
 
         while (true) {
-            if (!oldTrain.isEmpty() && oldTrain.peek() != 0) {
+            if (!oldTrain.isEmpty() && oldTrain.peek() != 0)             
                 newTrain.push(oldTrain.pop());
-            }
             else {
-                while (!oldTrain.isEmpty() && oldTrain.peek() == 0) {
-                    oldTrain.pop();                           // ги вади сите расипани (0)
-                }
+                while (!oldTrain.isEmpty() && oldTrain.peek() == 0)
+                    oldTrain.pop();
                 if (oldTrain.isEmpty())
-                    out += "Site vagoni se rasipani";
+                    out.append("Site vagoni se rasipani!");
                 else
-                    newTrain.push(oldTrain.pop());            // зема прв функционален
+                    newTrain.push(oldTrain.pop());
             }
+
             while (!oldTrain.isEmpty()) {
-                while (!oldTrain.isEmpty() && oldTrain.peek() == 0) {
-                    oldTrain.pop();                           // чистиме дополнителни 0-ки
-                }
-                if (oldTrain.isEmpty()) {
+                while (!oldTrain.isEmpty() && oldTrain.peek() == 0)
+                    oldTrain.pop();
+                if (oldTrain.isEmpty())
                     break;
-                }
                 else {
-                    top = oldTrain.pop();                    // земаме следен функционален
+                    top = oldTrain.pop();
                     if (newTrain.peek() < top) {
-                        shina.enqueue(newTrain.pop());       // врвот на novVoz е помал → го преместуваме во шина
-                        newTrain.push(top);                  // новиот поголем оди на врв на novVoz
-                    } else {
-                        shina.enqueue(top);                  // помалиот оди во шината
-                    }
+                        shina.enqueue(newTrain.pop());
+                        newTrain.push(top);
+                    } else 
+                        shina.enqueue(top);
                 }
             }
-            if (!shina.isEmpty()) {
-                newTrain.push(shina.dequeue());              // прв вагон од шината оди во novVoz
-            }
+            if (!shina.isEmpty())
+                newTrain.push(shina.dequeue());
             while (!shina.isEmpty()) {
                 top = shina.dequeue();
                 if (newTrain.peek() < top) {
-                    oldTrain.push(newTrain.pop());          // враќање на погрешно сортирани вагони
-                    newTrain.push(top);                     // новиот поголем оди прв
-                } else {
-                    oldTrain.push(top);                     // враќање назад за понатамошна обработка
-                }
+                    oldTrain.push(newTrain.pop());
+                    newTrain.push(top);
+                } else
+                    oldTrain.push(top);
             }
             if (oldTrain.isEmpty() && shina.isEmpty())
                 break;
         }
-        while (!newTrain.isEmpty()) {
-            out += newTrain.pop() + " ";
-        }
+        while (!newTrain.isEmpty())
+            out.append(newTrain.pop() + " ");
+
         return out;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(input.readLine());
-        String[] lines = new String[n];
 
-        for (int i = 0; i < n; i++) {
-            lines[i] = input.readLine();
-        }
+        int n = Integer.parseInt(input.readLine());
+        String[] line = new String[n];
+        for (int i = 0; i < n; i++)
+            line[i] = input.readLine();
         input.close();
 
-        System.out.println(preRaspredeleni(lines));
+        System.out.println(preRaspredeleni(line));
     }
 }
