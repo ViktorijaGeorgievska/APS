4cabefffb97ea09a5a0be18e3c664ebfddc5e314
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-21 00:40:13 +0200

    Update CallFunctions.java

--- Stack/CallFunctions.java
+++ Stack/CallFunctions.java
@@ -1,7 +1,13 @@
-/*
-Најди ја фунцкијата што повикува најмногу фунцкии во неа. Секоја фунцкија има Call x - x е името на фунцкијата - и Return.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-Input
+/*
+Prv kolokvium Povisoka 2024
+Најди ја фунцкијата што повикува највеќе фунцкии во неа.
+Секоја фунцкија има Call x - x е името на фунцкијата - и Return.
+
+Input:
 12
 Call a
 Call b
@@ -16,12 +22,53 @@ Call f
 Return
 Return
 
-Output
+Output:
 b 3
-
 Објаснување: во b се повикани c d и e
 */
 
+class Function {
+    String name;
+    int numCalls = 0;
+
+    public Function(String name) {
+        this.name = name;
+    }
+}
+
 public class CallFunctions {
-    public static void main(String[] args) {}
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(input.readLine());
+        ArrayStack<Function> functionsStack = new ArrayStack<>(n);
+        SLL<Function> helper = new SLL<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] line = input.readLine().split(" ");
+            if (line[0].equals("Call")) {
+                if (!functionsStack.isEmpty())
+                    functionsStack.peek().numCalls++;
+                functionsStack.push(new Function(line[1]));
+            } else if (line[0].equals("Return")) {
+                if (!functionsStack.isEmpty()) {
+                    Function f = functionsStack.pop();
+                    helper.insertLast(f);
+                }
+            }
+        }
+        while (!functionsStack.isEmpty())
+            helper.insertLast(functionsStack.pop());
+
+        input.close();
+
+        SLLNode<Function> current = helper.getFirst();
+        SLLNode<Function> maxNode = current;
+        while (current != null) {
+            if (current.element.numCalls >= maxNode.element.numCalls)
+                maxNode = current;
+            current = current.succ;
+        }
+        System.out.println(maxNode.element.name + " " + maxNode.element.numCalls);
+    }
 }
