f0b2cc81a73ca5e8e207317f3e9ccda4af2579dc
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-20 22:58:52 +0200

    Update IsValid.java

--- Stack/IsValid.java
+++ Stack/IsValid.java
@@ -1,42 +1,33 @@
 import java.util.Scanner;
 
 public class IsValid {
-    public static boolean isValid (SLL<String> functions) {
+    public static boolean isValid(SLL<String> functions) {
         SLLNode<String> current = functions.getFirst();
         LinkedStack<String> stack = new LinkedStack<>();
 
         while (current != null) {
-            if (current.element.startsWith("func")) {
+            if (current.element.startsWith("func"))
                 stack.push(current.element);
-            }
             else if (current.element.startsWith("end")) {
                 if (!stack.isEmpty()) {
-                    if (stack.peek().equals(current.element.substring(3))) {
+                    if (stack.peek().equals(current.element.substring(3)))
                         stack.pop();
-                    }
-                }
-                else {
+                } else
                     return false;
-                }
             }
             current = current.succ;
         }
-        if (stack.isEmpty())
-            return true;
-        else
-            return false;
+        return (stack.isEmpty());
     }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         SLL<String> functions = new SLL<>();
 
-        while (true) {
-            String line = in.nextLine();
-            if (line.equals("x"))
-                break;
-            functions.insertLast(line);
-        }
+        Scanner in = new Scanner(System.in);
+        String input;
+        while (!(input = in.next()).equals("x"))
+            functions.insertLast(input);
+        in.close();
 
         if (isValid(functions))
             System.out.println("Valid");
