9fd13b5d495d8e24d4031137fab0114c41fa825c
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-22 14:22:29 +0200

    Update CBHTAnagrams1.java

--- HashTable/CBHTAnagrams1.java
+++ HashTable/CBHTAnagrams1.java
@@ -18,7 +18,7 @@ Output:
 2
 */
 
-public class Anagrams1 {
+public class CBHTAnagrams1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -45,3 +45,4 @@ public class Anagrams1 {
             System.out.println("/");
     }
 }
+
