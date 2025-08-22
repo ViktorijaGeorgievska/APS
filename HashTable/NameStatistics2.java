a13eb9c0988db7b5d7ee23a40bfaaeb334e7dab3
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-22 12:59:20 +0200

    Update NameStatistics2.java

--- HashTable/NameStatistics2.java
+++ HashTable/NameStatistics2.java
@@ -1,8 +1,10 @@
-import java.util.Objects;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /*
-Input
+APS book
+Input:
 7
 Hristina F
 Magdalena F
@@ -18,7 +20,7 @@ Kristina
 Anastasija
 END
 
-Output
+Output:
 MAKEDONKA
 MAGDALENA
 No such name
@@ -29,95 +31,98 @@ ANA
 No such name
 */
 
-class Name implements Comparable<Name>{
+class Name implements Comparable<Name> {
     String name;
 
-    public Name(String name){
+    public Name(String name) {
         this.name = name.toUpperCase();
     }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Name name1 = (Name) o;
-        return Objects.equals(name, name1.name);
-    }
+
     @Override
     public int hashCode() {
         return (100 * name.charAt(0) + name.charAt(1)) % 9091;
     }
-    @Override
-    public String toString() {
-        return name;
-    }
+
     @Override
     public int compareTo(Name o) {
         return name.compareTo(o.name);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Name temp = (Name) obj;
+        return this.name.equals(temp.name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
 
 public class NameStatistics2 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = input.nextInt();
-        CBHT<String, Integer> hashTableF = new CBHT<>(9091);
-        CBHT<String, Integer> hashTableM = new CBHT<>(9091);
+        CBHT<Name, Integer> femaleHashtable = new CBHT<>(9091);
+        CBHT<Name, Integer> maleHashtable = new CBHT<>(9091);
+
+        int n = Integer.parseInt(in.readLine());
         for (int i = 0; i < n; i++) {
-            String name = input.next();
-            String pol = input.next();
+            String[] input = in.readLine().split(" ");
+            String name = input[0];
+            String pol = input[1];
+            Name newName = new Name(name.toUpperCase());
 
-            if (pol.compareTo("F") == 0) {
-                SLLNode<MapEntry<String, Integer>> female = hashTableF.search(name);
-                if (female == null) {
-                    hashTableF.insert(name, 1);
-                }
-                else {
-                    hashTableF.insert(name, female.element.value + 1);
-                }
-            }
-            else if (pol.compareTo("M") == 0) {
-                SLLNode<MapEntry<String, Integer>> male = hashTableM.search(name);
-                if (male == null) {
-                    hashTableM.insert(name, 1);
-                }
-                else {
-                    hashTableM.insert(name, male.element.value + 1);
-                }
+            if (pol.equals("F")) {
+                SLLNode<MapEntry<Name, Integer>> searchNode = femaleHashtable.search(newName);
+                if (searchNode != null)
+                    femaleHashtable.insert(newName, searchNode.element.value + 1);
+                else
+                    femaleHashtable.insert(newName, 1);
+            } else if (pol.equals("M")) {
+                SLLNode<MapEntry<Name, Integer>> searchNode = maleHashtable.search(newName);
+                if (searchNode != null)
+                    maleHashtable.insert(newName, searchNode.element.value + 1);
+                else
+                    maleHashtable.insert(newName, 1);
             }
         }
+        String searchPol = in.readLine().toUpperCase();
+        String searchName = in.readLine().toUpperCase();
 
-        String checkPol = input.next();
-        while (true) {
-            String names = input.next();
-            if (names.equals("END"))
-                break;
+        while (!searchName.equals("END")) {
+            if (searchPol.compareTo("F") == 0) {
+                SLLNode<MapEntry<Name, Integer>> resultF1 = femaleHashtable.getFirst(new Name(searchName));
+                SLLNode<MapEntry<Name, Integer>> currentF;
 
-            if (checkPol.equals("F")) {
-                SLLNode<MapEntry<String, Integer>> currentFemale = hashTableF.search(names);
-                if (currentFemale != null) {
-                    System.out.println(names.toUpperCase());
-                    System.out.println("F" + " " + names.toUpperCase() + " " + currentFemale.element.value);
-                }
-                else {
+                for (currentF = resultF1; currentF != null; currentF = currentF.succ)
+                    System.out.println(currentF.element.key.name);
+
+                SLLNode<MapEntry<Name, Integer>> resultF = femaleHashtable.search(new Name(searchName));
+                if (resultF != null)
+                    System.out.println(searchPol + " " + resultF.element.key.toString() + " " + resultF.element.value.toString());
+                else
                     System.out.println("No such name");
-                }
             }
-            else if (checkPol.equals("M")) {
-                SLLNode<MapEntry<String, Integer>> currentMale = hashTableM.search(names);
-                if (currentMale != null) {
-                    System.out.println(names.toUpperCase());
-                    System.out.println("M" + " " + names.toUpperCase() + " " + currentMale.element.value);
-                }
-                else {
+            if (searchPol.compareTo("M") == 0) {
+                SLLNode<MapEntry<Name, Integer>> resultM1 = maleHashtable.getFirst(new Name(searchName));
+                SLLNode<MapEntry<Name, Integer>> currentM;
+
+                for (currentM = resultM1; currentM != null; currentM = currentM.succ)
+                    System.out.println(currentM.element.key.name);
+
+                SLLNode<MapEntry<Name, Integer>> resultM = maleHashtable.search(new Name(searchName));
+                if (resultM != null)
+                    System.out.println(searchPol + " " + resultM.element.key.toString() + " " + resultM.element.value.toString());
+                else
                     System.out.println("No such name");
-                }
             }
+            searchName = in.readLine().toUpperCase();
         }
     }
 }
