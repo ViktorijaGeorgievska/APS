f72690fbf139706acf8b25db7a0e8c7e661284b4
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-22 14:09:48 +0200

    Update PhoneBook.java

--- HashTable/PhoneBook.java
+++ HashTable/PhoneBook.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /*
+APS book
 Input:
 3
 070111222 IvanIvanoski
@@ -19,15 +20,14 @@ Input:
 +38977111222
 Output: Unknown number
 */
-// APS book
+
+// прво решение
 public class PhoneBook {
     public static String normalizePhoneNumber(String phoneNumber) {
-        if (phoneNumber.startsWith("0")) {
+        if (phoneNumber.startsWith("0")) 
             phoneNumber = phoneNumber.substring(1);
-        }
-        else if (phoneNumber.startsWith("+")) {
+        else if (phoneNumber.startsWith("+")) 
             phoneNumber = phoneNumber.substring(4);
-        }
         return phoneNumber;
     }
 
@@ -48,11 +48,35 @@ public class PhoneBook {
         searchNumber = normalizePhoneNumber(searchNumber);
 
         SLLNode<MapEntry<String, String>> searchNode = phones.search(searchNumber);
-        if (searchNode == null) {
+        if (searchNode == null) 
             System.out.println("Unknown number");
-        }
-        else {
+        else 
             System.out.println(searchNode.element.value);
+    }
+}
+
+// второ решение
+public class CBHTPhoneBook2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(in.readLine());
+        CBHT<String, String> phoneBook = new CBHT<>(n * 2 + 1);
+        for (int i = 0; i < n; i++) {
+            String[] input = in.readLine().split(" ");
+            String number = input[0];
+            String name = input[1];
+
+            phoneBook.insert(number, name);
         }
+        String searchNumber = in.readLine();
+        if (searchNumber.startsWith("+389"))
+            searchNumber = searchNumber.replace("+389", "0");
+
+        SLLNode<MapEntry<String, String>> result = phoneBook.search(searchNumber);
+        if (result == null)
+            System.out.println("Unknown number");
+        else
+            System.out.println(result.element.value);
     }
 }
