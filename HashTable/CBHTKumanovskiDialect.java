fb26e8eb30f01f4c8bcf5a21ebf8bc3af8ac94f6
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-24 14:56:36 +0200

    Update and rename KumanovskiDialect.java to CBHTKumanovskiDialect.java

--- HashTable/CBHTKumanovskiDialect.java
+++ HashTable/CBHTKumanovskiDialect.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class KumanovskiDialect {
+// Courses
+
+public class CBHTKumanovskiDialect {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -62,3 +64,4 @@ public class KumanovskiDialect {
         System.out.println(correctText);
     }
 }
+
